27f2c7eda6178e5d295191b9b9a6f8abadc9cace
Jinwoo Jeong <jw.jeong@keti.re.kr>
2018-09-04 20:29:56 +0900

    aom build complete

---
+++ AV1/Source/main.c
@@ -0,0 +1,11 @@
+#include "av1/decoder/obu.h"
+
+int main()
+{
+	struct AV1Decoder *pbi = NULL;
+	const uint8_t *data = NULL;
+	const uint8_t *data_end = NULL;
+	const uint8_t **p_data_end = NULL;
+	aom_decode_frame_from_obus(pbi, data, data_end, p_data_end);
+	return 1;
+}
