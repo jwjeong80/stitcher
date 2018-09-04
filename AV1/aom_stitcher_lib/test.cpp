81879ce464db37f25846b675869f04064e991aee
Jinwoo Jeong <jw.jeong@keti.re.kr>
2018-09-04 21:59:41 +0900

    aom_stitcher project

---
+++ AV1/aom_stitcher_lib/test.cpp
@@ -0,0 +1,12 @@
+#include "av1/decoder/obu.h"
+
+#pragma comment(lib, "x64\Debug\AV1.lib")
+int main()
+{
+	//struct AV1Decoder *pbi = NULL;
+	//const uint8_t *data = NULL;
+	//const uint8_t *data_end = NULL;
+	//const uint8_t **p_data_end = NULL;
+	//aom_decode_frame_from_obus(pbi, data, data_end, p_data_end);
+	return 1;
+}
